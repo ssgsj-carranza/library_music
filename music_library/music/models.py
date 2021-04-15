6b36fa793564b90e21886e2fc9117e7c47f2518d
unknown <ssgsj.carranza@gmail.com>
2021-04-15 11:57:49 -0500

    installed django, started music project and made music app

---
+++ music_library/music/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+class Song(models.Model):
+    title = models.CharField(max_length=50)
+    artist = models.CharField(max_length=50)
+    albumn = models.CharField(max_length=50)
+    release_date = models.DateTimeField()
