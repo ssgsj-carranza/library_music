fb9487009be858ead1a4484f220e1c576c5a8ccb
unknown <ssgsj.carranza@gmail.com>
2021-04-20 11:45:45 -0500

    installed react and created components for music library

--- music_library/music_library/settings.py
+++ music_library/music_library/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'music.apps.MusicConfig'
+    'music.apps.MusicConfig',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -49,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
 ]
 
 ROOT_URLCONF = 'music_library.urls'
@@ -127,3 +129,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+CORS_ORIGIN_ALLOW_ALL=True
---
+++ music_list/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import reactDom from 'react-dom';
+import ReactDOM from 'react-dom';
+
+const jsxElement = <h1>Our react app</h1>;
+console.log(jsxElement);
+
+reactDom.render(jsxElement, document.getElementById('root'));
