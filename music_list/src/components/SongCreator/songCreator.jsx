7e56691add08d254c5cce97d9243a62ffd293584
unknown <ssgsj.carranza@gmail.com>
2021-04-21 09:22:31 -0500

    created two functions under the class song creator to handle the changes occuring and the submit occuring

--- music_list/src/components/SongCreator/songCreator.jsx
+++ music_list/src/components/SongCreator/songCreator.jsx
@@ -17,17 +17,28 @@ class SongCreator extends Component {
                     <div className="row col-align">
                         <div className="col-md-4">
                             <label>Title:</label>
-                            <input type=" text" name="title" value={TouchList.state.title}
+                            <input type=" text" name="title" value={Tthis.state.title}
                             onChange={}/>
                         </div>
                         <div className="col=md-4">
                             <label>Artist:</label>
-                            <input type=" text" name="artist" value={TouchList.state.artist}
+                            <input type=" text" name="artist" value={this.state.artist}
                             onChange={}/>
                         </div>
-
+                        <div className="col-md-4">
+                            <label>Album:</label>
+                            <input type="text" name="album" value={this.state.album}
+                            onChange={}/>
+                        </div>
+                        <div className="col=md=4">
+                            <label>Release Date:</label>
+                            <input type="text" name="release_date" value={this.state.release_date}
+                            onChange={}/>
+                        </div>
+                        <div className="col-md-4">
+                            <input type="submit" value="Add"/>
+                        </div>
                     </div>
-
                 </form>
             </div>
         );
